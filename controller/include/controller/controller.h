65e509a461fb38d07e4eb2173ba4265002081c55
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-12-01 17:17:04 -0500

    [Task 1.1b] Stub implementation of controller class

--- controller/include/controller/controller.h
+++ controller/include/controller/controller.h
@@ -1,1 +1,28 @@
 #include <ros/ros.h>
+
+namespace cleanup {
+
+class Controller {
+ public:
+  Controller();
+
+  /* @brief Begin exploration behavior. */
+  void explore();
+
+  /* @brief Begin cleaning behavior. */
+  void clean();
+
+  /* @brief Stop all motion. */
+  void stop();
+
+ private:
+
+  /* @brief Private method controlling overall exploration processing. */
+  void exploreLoop();
+
+  /* @brief Private method controlling overall cleaning processing. */
+  void cleanLoop();
+
+};
+
+} // namespace cleanup
