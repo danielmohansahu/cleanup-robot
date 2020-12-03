1c6ef5bb6dd583fbdae66ac983867a39af2c3c12
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-12-03 08:02:36 -0500

    [Task 2.2] Instantiated Navigation Node executable with Navigation class instantiation (and ROS node).

--- navigation/src/navigation_node.cpp
+++ navigation/src/navigation_node.cpp
@@ -3,5 +3,15 @@
 #include <navigation/navigation.h>
 
 int main(int argc, char** argv) {
+
+  // start ros node
+  ros::init(argc, argv, "navigation_node");
+
+  // instantiate Navigation class
+  cleanup::Navigation nav;
+  
+  // spin until shutdown
+  ros::spin();
+
   return 0;
 }
