d9b1f6e8b15880d7a89c96d02a5a2451b86837f6
selyard <spencer.elyard@gmail.com>
2020-12-05 08:40:52 -0500

    [Task 2.1] Stub test files, draft navigation test.

---
+++ test/controller.cpp
@@ -0,0 +1,14 @@
+#include "controller.h"
+#include <gtest/gtest.h>
+
+TEST(ControllerTest, TestExplore) {
+
+}
+
+TEST(ControllerTest, TestClean) {
+
+}
+
+TEST(ControllerTest, TestStop) {
+
+}
---
+++ test/perception.cpp
@@ -0,0 +1,18 @@
+#include "perception.h"
+#include <gtest/gtest.h>
+
+TEST(PerceptionTest_TestDetectObjects, should_pass) {
+
+}
+
+TEST(PerceptionTest_TestGetObjectPose, should_pass) {
+
+}
+
+TEST(PerceptionTest_TestImageCallback, should_pass) {
+
+}
+
+TEST(PerceptionTest_TestRunVisionAlgo, should_pass) {
+
+}
