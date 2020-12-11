81d90f6211a1d4f20fb78c9423f099a8defb1deb
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-12-11 15:30:00 -0500

    [Task 2.1] Fixed construction ordering issue.

--- navigation/test/navigation_test.cpp
+++ navigation/test/navigation_test.cpp
@@ -9,15 +9,15 @@
 
 #include <navigation/SetPoseStamped.h>
 
-cleanup::Navigation nav;
+std::unique_ptr<cleanup::Navigation> nav;
 std::shared_ptr<ros::NodeHandle> nh;
 
 TEST(NavigationTest_getNavModeDefault, should_pass) {;
-  EXPECT_EQ(nav.getCurrNavMode(),0);
+  EXPECT_EQ(nav->getCurrNavMode(),0);
 }
 
 TEST(NavigationTest_GetPose, should_pass) {
-   geometry_msgs::Pose curr_pose = nav.getRobotPose();
+   geometry_msgs::Pose curr_pose = nav->getRobotPose();
    EXPECT_FLOAT_EQ(curr_pose.position.x,0);
    EXPECT_FLOAT_EQ(curr_pose.position.y,0);
    EXPECT_FLOAT_EQ(curr_pose.position.z,0);
@@ -29,22 +29,23 @@ TEST(NavigationTest_GetPose, should_pass) {
 
 TEST(NavigationTest_stopServiceStarts, should_pass) {
   ros::ServiceClient client = nh->serviceClient<std_srvs::Trigger>("/navigation/stop");
-  EXPECT_EQ(nav.getCurrNavMode(),0);
+  EXPECT_EQ(nav->getCurrNavMode(),0);
 }
 
 TEST(NavigationTest_exploreServiceStarts, should_pass) {
   ros::ServiceClient client = nh->serviceClient<std_srvs::Trigger>("/navigation/explore");
-  EXPECT_EQ(nav.getCurrNavMode(),1);
+  EXPECT_EQ(nav->getCurrNavMode(),1);
 }
 
 TEST(NavigationTest_gotoServiceStarts, should_pass) {
   ros::ServiceClient client = nh->serviceClient<navigation::SetPoseStamped>("/navigation/goto");
-  EXPECT_EQ(nav.getCurrNavMode(),2);
+  EXPECT_EQ(nav->getCurrNavMode(),2);
 }
 
 int main(int argc, char **argv){
    ros::init(argc,argv, "navigation_test");
    nh.reset(new ros::NodeHandle);
+   nav.reset(new cleanup::Navigation);
    testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
