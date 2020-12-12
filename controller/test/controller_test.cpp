95464eaeb29c6a117aadef9468bb3dfe7a65a19d
selyard <spencer.elyard@gmail.com>
2020-12-11 22:17:04 -0500

    [Task 2.1] Initial draft update to controller test.

--- controller/test/controller_test.cpp
+++ controller/test/controller_test.cpp
@@ -1,13 +1,18 @@
 #include <ros/ros.h>
 #include <controller/controller.h>
 #include <gtest/gtest.h>
+#include <controller/SetModeAction.h>
 
-cleanup::Controller ctrl;
+std::unique_ptr<cleanup::Controller> ctrl;
+std::shared_ptr<ros::NodeHandle> nh;
 
 TEST(Controller_TestExecute_BadGoal, should_fail) {
    // send command to "blank"
    // TODO: make "goal" message
    //ctrl.executeGoal(controller::SetModeGoal::ConstPtr& goal);
+   Controller::SetModeGoal goal;
+   goal.mode = "errormode";
+   Controller::executeGoal(goal&);
 }
 TEST(Controller_TestExecute_CleanGoal, should_pass) {
    // send command to "clean"
@@ -22,6 +27,9 @@ TEST(Controller_TestExecute_ExploreGoal, should_pass) {
 
 
 int main(int argc, char **argv){
-   testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+  ros::init(argc,argv, "controller_test");
+  nh.reset(new ros::NodeHandle);
+  ctrl.reset(new cleanup::Controller);
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
