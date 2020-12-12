8473a476ddaec02dcf1123d135cd0f41d32cbe9c
selyard <spencer.elyard@gmail.com>
2020-12-12 07:46:28 -0500

    [Task 2.1] Draftr update to controller test.

--- controller/test/controller_test.cpp
+++ controller/test/controller_test.cpp
@@ -2,18 +2,33 @@
 #include <controller/controller.h>
 #include <gtest/gtest.h>
 #include <controller/SetModeAction.h>
+#include <actionlib/client/simple_action_client.h>
 
-std::unique_ptr<cleanup::Controller> ctrl;
 std::shared_ptr<ros::NodeHandle> nh;
+std::unique_ptr<cleanup::Controller> ctrl;
+actionlib::SimpleActionClient<controller::SetModeAction> client_;
 
-TEST(Controller_TestExecute_BadGoal, should_fail) {
+TEST(Controller_TestServicesExist, should_pass) {
+   //EXPECT_TRUE(ros::service::exists("navigation/goto",true));
+   //EXPECT_TRUE(ros::service::exists("navigation/stop",true));
+   //EXPECT_TRUE(ros::service::exists("navigation/explore",true));
+}
+
+TEST(Controller_TestActionClient_BadGoal, should_fail) {
    // send command to "blank"
    // TODO: make "goal" message
    //ctrl.executeGoal(controller::SetModeGoal::ConstPtr& goal);
-   Controller::SetModeGoal goal;
-   goal.mode = "errormode";
-   Controller::executeGoal(goal&);
+
+   // construct goal object
+   cleanup::controller::SetModeGoal goal;
+   goal.mode = "fail";
+   client_.sendGoal(goal);
+   //actionlib::SimpleActionClient<cleanup::Controller> ac("controller/set_mode",true);
+   //controller::SetModeGoal test_goal;
+   //test_goal.mode = "errormode";
+   //ac.sendGoal(test_goal);
 }
+
 TEST(Controller_TestExecute_CleanGoal, should_pass) {
    // send command to "clean"
    // make "goal" message
